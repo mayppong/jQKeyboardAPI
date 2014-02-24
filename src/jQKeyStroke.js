c42b6ab53c894971e817c382d6aa3649f2f3f481
May P <mayppong@gmail.com>
2014-02-23 19:23:24 -0800

    Clean up source file
    Switch from if-else structure to use switch case instead.
    Stop writing key data into the body tag's data.
    Clean up the logic checking variables.

--- src/jQKeyStroke.js
+++ src/jQKeyStroke.js
@@ -1,63 +1,54 @@
 
-function jQKeyStroke(event) {
+var jQKeyStroke = function(event) {
 
-	var keyData = {
-		eventType	: event.type,
-		keyCode		: event.which,
-		altKey		: event.altKey || false,
-		ctrlKey		: event.ctrlKey || false,
-		shiftKey	: event.shiftKey || false,
-		character	: "",
-		keyType		: null
-	};
-	
-	// Process keydown event.
-	// Pressing CTRL+ or ALT+ combination will causes the key to fire only on keydown and NOT keypress
-	if( keyData.eventType === "keydown" )
-	{
-		// Logic
-		var bNoModifierKey = !keyData.ctrlKey && !keyData.altKey;
-		var bModifierKey   =  keyData.ctrlKey ||  keyData.altKey;
-		var bIsCharKey 	  = 
-			( 48 <= keyData.keyCode && keyData.keyCode <=  90) ||
-			( 96 <= keyData.keyCode && keyData.keyCode <= 111) ||
-			(186 <= keyData.keyCode && keyData.keyCode <= 222);
-		var ignoreKeyList = [ 16, 17, 18 ]; 		// ALT, CTRL and SHIFT
-		
-		// End function if ignored key or normal character key is pressed to move on to keypress event
-		var bSkipKeyDown = (ignoreKeyList.indexOf(keyData.keyCode) > -1) || (bNoModifierKey && bIsCharKey);
-		if( bSkipKeyDown ) { return; }
-		
-		
-		if( bNoModifierKey ) {
-			if( keyData.keyCode <= 46 )
-			{
-				keyData.keyType = "COMMAND_KEY";
-			}
-			else if( 112 <= keyData.keyCode && keyData.keyCode <= 123 )
-			{
-				keyData.keyType = "FUNCTION_KEY";
-			}
-		}
-		else if( bModifierKey ) {
-			keyData.keyType = "MODIFIED_KEY";
-		}
-		
-		// Stop keypress from happening and start processing keys
-		//event.preventDefault();
-	}
-	// Processs keypress event.
-	else if( keyData.eventType === "keypress" )
-	{
-		keyData.character = String.fromCharCode( keyData.keyCode ); 
-		keyData.keyType   = "CHARACTER_KEY";
-	}
+    var keyData = { 
+        event     : event,
+        which     : event.which,
+        altKey    : event.altKey || false,
+        ctrlKey   : event.ctrlKey || false,
+        shiftKey  : event.shiftKey || false,
+        char      : null,
+        category  : null
+    };
+    
+    // Process keydown event.
+    // Pressing CTRL+ or ALT+ combination will causes the key to fire only on keydown and NOT keypress
+    if( event.type === "keydown" )
+    {
+        // Logic
+        var isModifierKey =  keyData.ctrlKey || keyData.altKey,
+            isCharKey     = 
+                ( 48 <= keyData.which && keyData.which <=  90) ||
+                ( 96 <= keyData.which && keyData.which <= 111) ||
+                (186 <= keyData.which && keyData.which <= 222),
+            isCommandKey = keyData.which <= 46;
+            isFunctionKey = 112 <= keyData.which && keyData.which <= 123,    
+            isIgnoreKey  = jQuery.inArray( keyData.which, [ 16, 17, 18 ] ) !== -1; // ALT, CTRL and SHIFT
+        
+        // End function if ignored key or normal character key is pressed to move on to keypress event
+        if( isIgnoreKey || (!isModifierKey && isCharKey) ) { 
+            return; 
+        }
+        
+        switch( true ) {
+            case isModifierKey:
+                keyData.keyType = "MODIFIER_KEY"; 
+                break;
+            case isCommandKey:
+                keyData.keyType = "COMMAND_KEY";
+                break;
+            case isFunctionKey:
+                keyData.keyType = "FUNCTION_KEY";
+                break;
+        }
 
-	jQuery("body").data("keyData", keyData);
-	jQuery(document).trigger("keystroke", keyData);
+    }
+    // Processs keypress event.
+    else if( event.type === "keypress" )
+    {
+        keyData.char    = String.fromCharCode( keyData.which ); 
+        keyData.keyType = "CHARACTER_KEY";
+    }
+
+    jQuery(event.target).trigger("keystroke", keyData);
 }
-
-
-jQuery( function() {
-	jQuery("body").on("keydown", jQKeyStroke ).on("keypress", jQKeyStroke );
-});
