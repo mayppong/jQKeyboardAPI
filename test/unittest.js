092b850be1186a9781c530577070569bde2c10a1
May P <mayppong@gmail.com>
2014-02-24 21:09:01 -0800

    Clean up test file.

--- test/unittest.js
+++ test/unittest.js
@@ -13,45 +13,57 @@ jQuery(document)
     });
 
 module( "Character Keys" );
-test( "Hit Letter a once", function() {
-    var testKeys = {
-        a_down: createKeyEvent( "keydown", { which: 65} ),
-        a_press: createKeyEvent( "keypress", {which: 97} ),
-        A_down: createKeyEvent( "keydown", {which: 65, shiftKey: true} ),
-    }
-    // Trigger event
-    jQuery("body").trigger(testKeys.a_down).trigger(testKeys.a_press);
-    
-    equal( KEYDATA.keyType, "CHARACTER_KEY", "Correct key type assigned" );
-    equal( KEYDATA.char, "a", "Corrent character code converted" );
-});
+    test( "Alpha characters", function() {
+        var testKeys = {
+            a_down:  createKeyEvent( "keydown", {which: 65} ),
+            a_press: createKeyEvent( "keypress", {which: 97} ),
+            A_down:  createKeyEvent( "keydown", {which: 65, shiftKey: true} ),
+            A_press: createKeyEvent( "keypress", {which: 65, shiftKey: true} )
+        }
+        
+        jQuery("body").trigger(testKeys.a_down).trigger(testKeys.a_press);
+        equal( KEYDATA.keyType, "CHARACTER_KEY", "Correct key type assigned" );
+        equal( KEYDATA.char, "a", "Corrent character code converted" );
 
-test( "Hit CTRL+V", function() {
-    var testKeys = {
-        v_down: null,
-        ctrl_down: null,
-        ctrl_v_down: createKeyEvent( "keydown", {which: 86, ctrlKey: true} ),
-        ctrl_v_press: createKeyEvent( "keypress", {which: 118, ctrlKey: true} )
-    };
-    jQuery("body").trigger(testKeys.ctrl_v_down).trigger(testKeys.ctrl_v_press);
+        jQuery("body").trigger(testKeys.A_down).trigger(testKeys.A_press);
+        equal( KEYDATA.keyType, "CHARACTER_KEY", "Correct key type assigned" );
+        equal( KEYDATA.shiftKey, true, "SHIFT key was pressed" );
+        equal( KEYDATA.char, "A", "Corrent character code converted" );
+    });
 
-    equal( KEYDATA.keyType, "CHARACTER_KEY", "Correct key type assigned" );
-    equal( KEYDATA.ctrlKey, true, "CTRL key was pressed" );
-});
+    test( "Alpha keys with modifier (short cuts)", function() {
+        var testKeys = {
+            v_down:       null,
+            ctrl_down:    null,
+            ctrl_v_down:  createKeyEvent( "keydown", {which: 86, ctrlKey: true} ),
+            ctrl_v_press: createKeyEvent( "keypress", {which: 118, ctrlKey: true} )
+        };
+        jQuery("body").trigger(testKeys.ctrl_v_down).trigger(testKeys.ctrl_v_press);
+
+        equal( KEYDATA.keyType, "CHARACTER_KEY", "Correct key type assigned" );
+        equal( KEYDATA.ctrlKey, true, "CTRL key was pressed" );
+        equal( KEYDATA.char, "v", "Corrent character code converted" );
+    });
 
 
 module( "Function Keys");
-test( "Hit F01 once", function() {
-    var testKeys = {
-        F1_down: createKeyEvent( "keydown", {which: 112} ),
-        F13_down: createKeyEvent( "keydown", {which: 112, shiftKey: true} ),
-        F1_CTRL_down: createKeyEvent( "keydown", {which: 112, ctrlKey: true} ),
-    };
-    // Trigger event
-    jQuery("body").trigger(testKeys.F1_down);
-    
-    equal( KEYDATA.keyType, "FUNCTION_KEY", "Correct key type assigned" );
-    equal( KEYDATA.char, null, "Corrent character code converted" );
+    test( "Hit F01 once", function() {
+        var testKeys = {
+            F1_down:      createKeyEvent( "keydown", {which: 112} ),
+            F13_down:     createKeyEvent( "keydown", {which: 112, shiftKey: true} ),
+            F1_CTRL_down: createKeyEvent( "keydown", {which: 112, ctrlKey: true} ),
+        };
+        // Trigger event
+        jQuery("body").trigger(testKeys.F1_down);
+        
+        equal( KEYDATA.keyType, "FUNCTION_KEY", "Correct key type assigned" );
+        equal( KEYDATA.char, null, "Corrent character code converted" );
+    });
+
+
+// Allow manual testing
+QUnit.done( function() {
+    jQuery(document).on( "keystroke", function(event) {
+        console.log(event);
+    });
 });
-
-
